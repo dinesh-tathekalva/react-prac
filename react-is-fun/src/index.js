2326cc3e0e0c2bbbdcb528893b444562fe7da2e1
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-05-16 00:41:31 -0500

    wip

--- react-is-fun/src/index.js
+++ react-is-fun/src/index.js
@@ -84,7 +84,7 @@ const Library = ({books}) => {
       <Book title='Big Bang theory' author='Stephan Hawking' pages={350} />
       <Book title='String Theory' author='Michio Kaku' pages={550} />
       {books.map(
-        book => <Book title={book.title} author={book.author} pages={book.author} />
+        (book, i) => <Book key={i} title={book.title} author={book.author} pages={book.author} />
       ) }
       
     </div>
